94e407ee00708d60f68f299df1fa6235f028e0ce
Wrspider <spider.wang@gmail.com>
2021-05-12 08:48:11 +0800

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormComponent } from './components/form/form.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+// 裝飾器
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule
+
+  ],
+  exports: [
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+
+  public peopleInfo: any = {
+    username: '',
+    sex: '1',
+    cityList: ['北京', '上海', '深圳', '杭州', '潍坊', '青岛', '厦门'],
+    city: '北京',
+    hobby: [
+      {
+        title: '吃饭',
+        checked: false
+      }, {
+        title: '睡觉',
+        checked: false
+      }, {
+        title: '看剧',
+        checked: false
+      }, {
+        title: '敲代码',
+        checked: true
+      }
+    ],
+    mark: ''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  doSubmit() {
+    // jquery dom 操作(第一种方法)
+    /*<input type="text" id="username"/>
+    let usernameDom:any=document.getElementById('username');
+    console.log(usernameDom.value);
+    */
+    // 第二种方法：双向数据绑定(好好理解，学会!)
+    console.log(this.peopleInfo);
+  }
+
+}
